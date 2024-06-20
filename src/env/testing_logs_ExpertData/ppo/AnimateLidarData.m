2b24edb49f290b9a89281bf68cf5db530e20aa9a
Wen-Chung (Andy) Cheng <wcheng3@fau.edu>
2024-06-19 23:06:02 -0400

    Added MATLAB scripts for visualizing collected expert data

--- src/env/testing_logs_ExpertData/ppo/AnimateLidarData.m
+++ src/env/testing_logs_ExpertData/ppo/AnimateLidarData.m
@@ -3,24 +3,120 @@
 % and the last dimension represents the x and y coordinates.
 % For this example, we'll just animate the first 100 points for simplicity.
 
-% Extract the x and y coordinates
-x = squeeze(LiDARData(1:34, :));
+ep = 1;
+lid_start = 1;
+lid_end = 0;
 
-% Create a new figure
-figure
+for i = 1:size(Episodes, 1)
 
-% Loop over the time steps
-for t = 1:size(x, 1)
-    % Clear the current figure
-    clf
+    disp(Episodes(i));
+    disp(lid_start);
 
-    % Plot the LiDAR points at the current time step
-    LidData_t = LiDARData(t, :);
-    angles = linspace(0,2*pi,numel(LidData_t(2:end)));
-    scan = lidarScan(LidData_t(2:end),angles);
+    if Episodes(i) == ep
 
-    plot(scan)
+        continue
 
-    % Pause for a short time to create the animation effect
-    pause(0.10)
+    else
+        lid_end = i;
+
+        disp(lid_end);
+
+
+        vidname = "LidarDataEp"+ string(ep) + ".mp4";
+
+        % Extract the x and y coordinates
+        x = squeeze(LiDARData(lid_start:lid_end, :));
+        
+        % Create a new figure
+        figure
+        
+        % set(gca,"NextPlot","replacechildren")
+        set(gcf,"NextPlot","replacechildren")
+        v = VideoWriter(vidname, 'MPEG-4');
+        
+        % decrease framerate 
+        v.FrameRate = 10;              
+        open(v)
+        
+        % Loop over the time steps
+        for t = lid_start:lid_end
+            % Clear the current figure
+            clf
+        
+            % Plot the LiDAR points at the current time step
+            LidData_t = LiDARData(t, :);
+            angles = linspace(0,2*pi,numel(LidData_t(2:end)));
+            scan = lidarScan(LidData_t(2:end),angles);
+        
+            plot(scan)
+        
+            hold on
+        
+            % % Define the radius of the circle
+            % radius = 0.2;
+            % 
+            % Define the center of the circle
+            centerX = 0;
+            centerY = 0;
+            % 
+            % % Calculate the left and bottom coordinates of the circle
+            % x = centerX - radius;
+            % y = centerY - radius;
+            % 
+            % % % Create a new figure
+            % % figure
+            % 
+            % % Plot the circle
+            % rectangle('Position', [x, y, 2*radius, 2*radius], 'Curvature', [1 1], 'FaceColor', 'r')
+
+
+
+            % Define the size of the triangle
+            size = 0.5;
+
+            % Calculate the coordinates of the vertices of the triangle
+            y = [centerX - size/2, centerX + size/2, centerX];
+            x = [centerY - size/2, centerY - size/2, centerY + size/2];
+
+            % % Create a new figure
+            % figure
+
+            % Plot the triangle
+            fill(x, y, 'r')
+
+
+        
+            hold off
+        
+            frame = getframe(gcf);
+            % frame = getframe(gca);
+            writeVideo(v,frame)
+        
+            % Pause for a short time to create the animation effect
+            pause(0.05)
+        end
+        
+        close(v)
+
+        ep = ep + 1;
+
+        lid_start = lid_end;
+
+        % pause(1)
+
+    end
+
+
 end
+
+
+
+
+% syms t
+% fig = figure;
+% 
+% syms t
+% fanimator(@fplot,cos(x)+t,sin(x)+1,[-pi pi])
+% axis equal
+% 
+% writeAnimation(fig,'loop.gif','LoopCount',1)
